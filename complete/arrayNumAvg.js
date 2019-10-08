0d3605451e038de8c2f0042cc710205e6fe02b7e
Dasol Park <dasol.park615@gmail.com>
2019-10-08 23:53:06 +0900

    Update ArrayNumAvg, Spy(typo)

--- complete/arrayNumAvg.js
+++ complete/arrayNumAvg.js
@@ -25,14 +25,20 @@
 
 // # 2nd solution(Final)
 
+// function solution(arr) {
+//   var answer = 0;
+
+//   for (i = 0; i < arr.length; i++) {
+//     answer += arr[i];
+//   }
+
+//   return answer / arr.length;
+// }
+
+// # 3rd solution(with reduce())
+
 function solution(arr) {
-  var answer = 0;
-
-  for (i = 0; i < arr.length; i++) {
-    answer += arr[i];
-  }
-
-  return answer / arr.length;
+  return arr.reduce((acc, curVal) => (acc += curVal)) / arr.length;
 }
 
 console.log(solution([1, 2, 3, 4]));
