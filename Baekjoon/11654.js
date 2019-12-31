2c58f2ea15236b75873c5f215996968ebc3c42e3
Dasol Park <dasol.park615@gmail.com>
2019-12-31 12:30:30 +0900

    ASCII Code(BJ Q.11654)

---
+++ Baekjoon/11654.js
@@ -0,0 +1,9 @@
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+// For Local Test
+const input = 'A\n';
+
+console.log(input.charCodeAt(0));
