b9164f18301aec45680c44eeb204a6500f53bcdd
Dasol Park <dasol.park615@gmail.com>
2020-01-03 13:31:37 +0900

    More on hansoo(BJ1065)

--- Baekjoon/1065.js
+++ Baekjoon/1065.js
@@ -1,3 +1,5 @@
+// Hansoo(category - function)
+
 // 1st Solution
 
 // For submit
@@ -75,3 +77,69 @@ console.log(hansoo(1000));
 // }
 
 // console.log(hansoo);
+
+// 2nd Solution
+
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+// For local test
+// const input = '1000';
+
+// function hanCheck(num) {
+//   num = parseInt(num);
+//   let counter = 0;
+//   for (let i = 1; i <= num; i++) {
+//     if (i < 100) {
+//       counter++;
+//     } else if (i < 1000) {
+//       let temp = i;
+//       let numArr = [];
+//       let index = 0;
+//       while (temp > 0) {
+//         numArr[index] = temp % 10;
+//         temp = parseInt(temp / 10);
+//         index++;
+//       }
+//       if (numArr[0] - numArr[1] === numArr[1] - numArr[2]) {
+//         counter++;
+//       }
+//     }
+//   }
+//   console.log(counter);
+// }
+
+// hanCheck(input);
+
+// 3rd Solution
+
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim();
+
+// For local test
+// const input = '110';
+// const N = parseInt(input);
+
+// function hanCheck(N) {
+//   let hansoo = 0;
+//   for (let i = 1; i <= N; i++) {
+//     if (i < 100) {
+//       hansoo++;
+//     } else {
+//       const numArr = i
+//         .toString()
+//         .split('')
+//         .map(num => parseInt(num));
+//       if (numArr[0] - numArr[1] === numArr[1] - numArr[2]) {
+//         hansoo++;
+//       }
+//     }
+//   }
+//   console.log(hansoo);
+// }
+
+// hanCheck(N);
