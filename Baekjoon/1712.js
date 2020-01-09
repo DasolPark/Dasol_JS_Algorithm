37d399b654275b31f94815eb86c2853ec8cc5b1b
Dasol Park <dasol.park615@gmail.com>
2020-01-09 17:37:43 +0900

    Break-Even Point(BJ Q.1712)

---
+++ Baekjoon/1712.js
@@ -0,0 +1,40 @@
+// Break-Even Point
+
+// 1st Solution(other)
+
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+
+// For local test
+const input = ['1000', '70', '170'];
+const A = parseInt(input.shift());
+const B = parseInt(input.shift());
+const C = parseInt(input.shift());
+const netProfit = C - B;
+
+if (netProfit <= 0) {
+  console.log(-1);
+} else {
+  console.log(Math.floor(A / netProfit) + 1);
+}
+
+// 2nd Solution(ohter)
+
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(num => parseInt(num));
+
+// For local test
+// const input = ['1000', '70', '170'].map(num => parseInt(num));
+// const A = input.shift();
+// const B = input.shift();
+// const C = input.shift();
+
+// if (C <= B) {
+//   console.log(-1);
+// } else {
+//   console.log(Math.floor(A / (C - B)) + 1);
+// }
