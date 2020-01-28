81db6ac69b602fdc6f2c24c6d9998a15b63b0df0
Dasol Park <dasol.park615@gmail.com>
2020-01-28 16:31:54 +0900

    Taxi Geometry(Euclid Geometry)(BJ Q.3053)

---
+++ Baekjoon/3053.js
@@ -0,0 +1,20 @@
+// Taxi geometry(Euclid geometry)
+
+// For submit
+
+// const fs = require('fs');
+// const input = parseInt(fs.readFileSync('/dev/stdin').toString().trim());
+
+// For local test
+const input = 1;
+
+function Euclid(radius) {
+  return Math.pow(radius, 2) * Math.PI;
+}
+
+function taxi(radius) {
+  return Math.pow(radius, 2) * 2;
+}
+
+console.log(Euclid(input).toFixed(6));
+console.log(taxi(input).toFixed(6));
