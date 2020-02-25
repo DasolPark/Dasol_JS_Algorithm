a1b04c2777c544aaf3d9853c4ea1e616d64bdde3
Dasol Park <dasol.park615@gmail.com>
2020-02-25 21:15:21 +0900

    Update console of 15649 and 15650

--- Baekjoon/15649.js
+++ Baekjoon/15649.js
@@ -1,5 +1,44 @@
 // N and M (1) Permutation
 
+// 1st Solution
+
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(nm => parseInt(nm));
+
+// For local test
+// const input = [4, 2];
+// const N = input.shift();
+// const M = input.shift();
+
+// const visited = new Array(N);
+// let output = [];
+
+// function print() {
+//   console.log(output.join(' '));
+// }
+
+// function dfs(cnt) {
+//   if (cnt === M) {
+//     print();
+//     return;
+//   }
+
+//   for (let i = 0; i < N; i++) {
+//     if (visited[i] === true) continue;
+//     visited[i] = true;
+//     output.push(i + 1);
+//     dfs(cnt + 1);
+//     output.pop();
+//     visited[i] = false;
+//   }
+// }
+
+// dfs(0);
+
+// 2nd Solution
+
 // For submit
 
 // const fs = require('fs');
@@ -12,14 +51,11 @@ const M = input.shift();
 
 const visited = new Array(N);
 let output = [];
-
-function print() {
-  console.log(output.join(' '));
-}
+let result = '';
 
 function dfs(cnt) {
   if (cnt === M) {
-    print();
+    result += `${output.join(' ')}\n`;
     return;
   }
 
@@ -34,3 +70,4 @@ function dfs(cnt) {
 }
 
 dfs(0);
+console.log(result.trim());
--- Baekjoon/15650.js
+++ Baekjoon/15650.js
@@ -6,15 +6,48 @@
 // const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(nm => parseInt(nm));
 
 // For local test
+// const input = [3, 1];
+// const N = input.shift();
+// const M = input.shift();
+// const visited = new Array(N);
+// const output = [];
+
+// function dfs(idx, cnt) {
+//   if (cnt === M) {
+//     console.log(output.join(' '));
+//     return;
+//   }
+
+//   for (let i = idx; i < N; i++) {
+//     if (visited[i] === true) continue;
+//     visited[i] = true;
+//     output.push(i + 1);
+//     dfs(i, cnt + 1);
+//     output.pop();
+//     visited[i] = false;
+//   }
+// }
+
+// dfs(0, 0);
+
+// 2nd Solution
+
+// For submit
+
+// const fs = require('fs');
+// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ').map(nm => parseInt(nm));
+
+// For local test
 const input = [3, 1];
 const N = input.shift();
 const M = input.shift();
 const visited = new Array(N);
 const output = [];
+let result = '';
 
 function dfs(idx, cnt) {
   if (cnt === M) {
-    console.log(output.join(' '));
+    result += `${output.join(' ')}\n`;
     return;
   }
 
@@ -29,3 +62,4 @@ function dfs(idx, cnt) {
 }
 
 dfs(0, 0);
+console.log(result);
